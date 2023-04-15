f49be393a0045fab3c32e20fbd6095a6b0260cb4
tenkei-a <k1.abe0423@gmail.com>
2023-04-15 16:57:24 +0900

    printf

--- lesson_string.py
+++ lesson_string.py
@@ -1,23 +1,50 @@
-s = 'test'
-print('hello')
-print("I don't know")
-print('Say "I don\'t know"')
+# s = 'test'
+# print('hello')
+# print("I don't know")
+# print('Say "I don\'t know"')
 
-print('hello. \nHow are you?')
+# print('hello. \nHow are you?')
 
-print(r'C:\name\name')
+# print(r'C:\name\name')
 
-print("#################")
-print("""\
-line1
-line2
-line3\
-""")
-print("#################")
+# print("#################")
+# print("""\
+# line1
+# line2
+# line3\
+# """)
+# print("#################")
 
-print('Hi.' * 3 + 'Mike')
+# print('Hi.' * 3 + 'Mike')
 
-print('py' + 'thon')
+# print('py' + 'thon')
 
-prefix = 'py'
-print(prefix + 'thon')
+# prefix = 'py'
+# print(prefix + 'thon')
+
+# s = 'My name is Mike. Hi Mike.'
+# print(s)
+
+# print('####################')
+# is_start = s.startswith('My')
+# print(is_start)
+# print(s.find('Mike'))
+# print(s.rfind('Mike'))
+# print(s.count('Mike'))
+# print(s.capitalize())
+# print(s.title())
+# print(s.upper())
+# print(s.lower())
+
+# print(s.replace('Mike','Nancy'))
+
+# f-strings
+a = 'a'
+print(f'a is {a}')
+
+x,y,z = 1,2,3
+print(f'a is {x},{y},{z}')
+
+name = 'k1'
+
+print(f'My name is {name}.')

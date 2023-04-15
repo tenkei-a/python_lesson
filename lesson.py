df07fd9d7f562f9d56f919b56bc0c9818c4b9e67
tenkei-a <k1.abe0423@gmail.com>
2023-04-15 14:53:45 +0900

    lesson

---
+++ lesson.py
@@ -0,0 +1,12 @@
+num = 1
+name = '1'
+
+# 型変換
+new_num = int(name)
+
+print(num,type(num))
+print(name,type(name))
+
+print('Hi','Mike',sep=',',end='.\n')
+print('Hi','Mike',sep=',',end='.\n')
+
---
+++ lesson_string.py
@@ -0,0 +1,23 @@
+s = 'test'
+print('hello')
+print("I don't know")
+print('Say "I don\'t know"')
+
+print('hello. \nHow are you?')
+
+print(r'C:\name\name')
+
+print("#################")
+print("""\
+line1
+line2
+line3\
+""")
+print("#################")
+
+print('Hi.' * 3 + 'Mike')
+
+print('py' + 'thon')
+
+prefix = 'py'
+print(prefix + 'thon')

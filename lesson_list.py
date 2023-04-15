95598ad457cee3634bcc34d5400da24e37e03ace
tenkei-a <k1.abe0423@gmail.com>
2023-04-15 17:05:49 +0900

    create lesson_list.py

---
+++ lesson_list.py
@@ -0,0 +1,15 @@
+l = [1,2,3,4,5,6,7,8,9]
+
+# print(l)
+# print(l[:2])
+# print(l[4:6])
+# print(l[::2])
+# print(l[::-2])
+
+a = ['a','b','c']
+n = [1,2,3,4]
+x = [a,n]
+print(x)
+print(x[0][2])
+print(x[1][:])
+

367d57f1b820010a8e349ff64c46ee0e1f5081bf
tenkei-a <k1.abe0423@gmail.com>
2023-04-17 12:02:03 +0900

    a

--- lesson_list.py
+++ lesson_list.py
@@ -6,10 +6,31 @@ l = [1,2,3,4,5,6,7,8,9]
 # print(l[::2])
 # print(l[::-2])
 
-a = ['a','b','c']
-n = [1,2,3,4]
-x = [a,n]
-print(x)
-print(x[0][2])
-print(x[1][:])
+# a = ['a','b','c']
+# n = [1,2,3,4]
+# x = [a,n]
+# print(x)
+# print(x[0][2])
+# print(x[1][:])
 
+s = ['a','b','c']
+s[0] = 'X'
+print(s)
+s[1:2] = []
+print(s)
+s.append('s')
+print(s)
+s.pop()
+print(s)
+
+
+s = [1,2,2,2,3]
+s.remove(2)
+print(s)
+
+a = [1,2,3,4,5]
+b = [6,7,8,9,10]
+x = a+b
+print(x)
+a += b
+print(a)

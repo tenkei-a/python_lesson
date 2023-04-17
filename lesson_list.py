fa66eba695a036d26e64f69ba43f9c6966fc4565
tenkei-a <k1.abe0423@gmail.com>
2023-04-17 12:10:12 +0900

    list

--- lesson_list.py
+++ lesson_list.py
@@ -1,4 +1,4 @@
-l = [1,2,3,4,5,6,7,8,9]
+# l = [1,2,3,4,5,6,7,8,9]
 
 # print(l)
 # print(l[:2])
@@ -13,24 +13,48 @@ l = [1,2,3,4,5,6,7,8,9]
 # print(x[0][2])
 # print(x[1][:])
 
-s = ['a','b','c']
-s[0] = 'X'
-print(s)
-s[1:2] = []
-print(s)
-s.append('s')
-print(s)
-s.pop()
-print(s)
+# s = ['a','b','c']
+# s[0] = 'X'
+# print(s)
+# s[1:2] = []
+# print(s)
+# s.append('s')
+# print(s)
+# s.pop()
+# print(s)
 
 
-s = [1,2,2,2,3]
-s.remove(2)
-print(s)
+# s = [1,2,2,2,3]
+# s.remove(2)
+# print(s)
 
-a = [1,2,3,4,5]
-b = [6,7,8,9,10]
-x = a+b
-print(x)
-a += b
-print(a)
+# a = [1,2,3,4,5]
+# b = [6,7,8,9,10]
+# x = a+b
+# print(x)
+# a += b
+# print(a)
+
+# r = [1,2,3,4,5,1,2,3]
+# print(r.index(3, 3))
+# print(r.count(3))
+
+# リストrに5が含まれていたらexist
+# if 5 in r:
+#     print('exist')
+
+# r.sort()
+# print(r)
+# r.sort(reverse=True)
+# print(r)
+# r.reverse
+# print(r)
+
+# s = 'My name is Mile.'
+# to_split = s.split(' ')
+# print(to_split)
+
+# x = ' '.join(to_split)
+# print(x)
+
+# print(help(list))

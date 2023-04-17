7a2ae91666385641441d73b6b13072dacaf8096c
tenkei-a <k1.abe0423@gmail.com>
2023-04-17 12:35:07 +0900

    create tuple

---
+++ lesson_tuple.py
@@ -0,0 +1,28 @@
+num_tuple = (10,20)
+print(num_tuple)
+
+x,y = num_tuple
+print(x,y)
+
+min, max = 0, 100
+print(min, max)
+
+i = 10
+j = 20
+tmp = i
+i = j
+j = tmp
+print(i, j)
+
+a = 100
+b = 200
+a, b = b, a
+print(a, b)
+
+
+chose_from_two = ('A','B','C')
+answer = []
+answer.append('A')
+answer.append('C')
+print(chose_from_two)
+print(answer)

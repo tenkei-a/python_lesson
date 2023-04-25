17920133027c1ed30d1a01ee51d057dacef9cbd2
tenkei-a <k1.abe0423@gmail.com>
2023-04-25 14:19:24 +0900

    a

---
+++ lesson_import.py
@@ -0,0 +1,11 @@
+# import lesson_package.utils
+
+from lesson_package import utils
+
+# r = lesson_package.utils.say_twice('hello')
+r = utils.say_twice('hello')
+
+print(r)
+
+# import sys
+# print(sys.path)
--- lesson_package.py
+++
@@ -1,4 +0,0 @@
-import lesson_package.utils
-
-# r = lesson_package.utils.say_twice('hello')
-# print(r)
